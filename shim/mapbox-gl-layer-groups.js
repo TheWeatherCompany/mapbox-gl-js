32db3306974dc2318b09870644223d6594fc4614
eliotj <eliot.johnston@weather.com>
2016-12-07 11:16:59 -0500

    fixing mistakes

--- shim/mapbox-gl-layer-groups.js
+++ shim/mapbox-gl-layer-groups.js
@@ -116,7 +116,8 @@ function getLayerFromIndex(map, index) {
 }
 
 function getLayerGroup(map, id) {
-    return map.getLayer(id).metadata.group;
+	var layer = map.getLayer(id);
+	if ("metadata" in layer && "group" in layer.metadata)	return map.getLayer(id).metadata.group;
 }
 
 function isLayer(map, id) {
